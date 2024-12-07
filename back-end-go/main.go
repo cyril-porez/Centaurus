3730c664fc40a888f35c72f1c95f8b69dd5d9181
cyril <cyril.porez@laplateforme.io>
2024-12-07 20:48:20 +0100

    initialisation backend go install lib swagger lib gin for api

--- back-end-go/main.go
+++ back-end-go/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	_ "back-end-go/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+//PingHandler
+//@Summary Ping
+// @Description Respond with "pong"
+// @Tags example
+// @Produce json
+// @Success 200 {string} string "pong"
+// @Router /ping [get]
+func PingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func  main()  {
-	fmt.Println("Hello World !")
+	r := gin.Default();
+
+	//route Swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	//route api
+	r.GET("/ping", PingHandler)
+	r.GET("/user/:id", func(c *gin.Context) {
+
+	})
+
+	r.Run()
 }
