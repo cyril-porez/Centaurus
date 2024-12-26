b44b671de7af5efc2102e205d21ebd95429f6d38
cyril <cyril.porez@laplateforme.io>
2024-12-26 13:36:05 +0100

    fix uri api

--- front-end/src/config/url_api.js
+++ front-end/src/config/url_api.js
@@ -1,10 +1,10 @@
-const address = "https://homalink-back-master.development.atelier.ovh";
-// const address = "http://localhost:1337";
+// const address = "https://homalink-back-master.development.atelier.ovh";
+const localAddress = "http://localhost:1337";
 
-export const URL = `${address}`;
+export const URL = `${localAddress}`;
 
-export const URL_LOGIN = `${address}/api/auth/local/`;
-export const URL_REGISTER = `${address}/api/auth/local/register`;
+export const URL_LOGIN = `${localAddress}/api/auth/local/`;
+export const URL_REGISTER = `${localAddress}/api/auth/local/register`;
 
-export const URL_HORSES = `${address}/api/horses/`;
-export const URL_HORSES_BY_USER = `${address}/api/users/4?populate[0]=horses`;
+export const URL_HORSES = `${localAddress}/api/horses/`;
+export const URL_HORSES_BY_USER = `${localAddress}/api/users/4?populate[0]=horses`;
