59ee7e94a5aaad7e6fa8cb714c3f69fc39aa1506
cyril <cyril.porez@laplateforme.io>
2025-01-19 21:12:43 +0100

    HOM-14 delete backend strapi and rename backend go in backend

--- back-end/src/api/horse/controllers/horse.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * horse controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::horse.horse');
--- back-end/src/api/horse/routes/horse.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * horse router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::horse.horse');
