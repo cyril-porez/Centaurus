01e795ef286cc7953465bad3824c182fe0474755
cyril <cyril.porez@laplateforme.io>
2025-02-18 15:06:20 +0100

    CEN-55 feat(dev-env): add docker setup for backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "⏳ Attente de MySQL..."
-until  nc -z -v -w30 mysql 3306; do
-  echo "⏳ MySQL n'est pas encore prêt..."
+echo "Attente de MySQL..."
+until nc -z -v -w30 mysql 3306; do
+  echo "MySQL n'est pas encore prêt..."
   sleep 2
 done
 
-echo "✅ MySQL est prêt, démarrage de l'application..."
+echo "MySQL est prêt, démarrage de l'application..."
 exec "$@"
