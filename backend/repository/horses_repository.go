0bb286c5f6d8585f5303246c13c3904521314c8b
Cyril Porez <cyril.porez@laplateforme.io>
2025-09-02 20:52:41 +0200

    CEN-67 fix(backend): problem query sql order value

--- backend/repository/horses_repository.go
+++ backend/repository/horses_repository.go
@@ -12,8 +12,11 @@ type SQLHorseRepository struct {}
 func (r *SQLHorseRepository) InsertHorse(db *sql.DB, horse *model.Horses) error{
 	horse.CreatedAt = time.Now().Format(time.RFC3339);
 	query := "INSERT INTO horses (name, age, race, fk_user_id, created_at) VALUES (?, ?, ?, ?, ?)";
+	log.Printf("[DEBUG] Insertion horse: name=%s, age=%d, race=%s, fk_user_id=%d", 
+		horse.Name, horse.Age, horse.Race, horse.FkUserId)
 	result, err := db.Exec(query, horse.Name, horse.Age, horse.Race, horse.FkUserId, horse.CreatedAt);
 	if err != nil {
+		log.Printf("[ERROR] SQL insertion failed: %v", err)
 		return err
 	}
 
